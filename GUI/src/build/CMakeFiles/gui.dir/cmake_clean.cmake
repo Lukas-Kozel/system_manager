ad8cee8755339ba85358de093c8f18402dcffa12
LukyKozlojc <l.kozel@outlook.cz>
2024-01-14 19:05:16 +0100

    added init part of gui app

---
+++ GUI/src/build/CMakeFiles/gui.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/gui_autogen.dir/ParseCache.txt"
+  "gui_autogen"
+  "CMakeFiles/gui.dir/gui_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/gui.dir/gui_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/gui.dir/main.cpp.o"
+  "CMakeFiles/gui.dir/main.cpp.o.d"
+  "gui"
+  "gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GUI/src/build/CMakeFiles/gui.dir/gui_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/gui.dir/gui_autogen/mocs_compilation.cpp.o: \
+ /home/luky/Desktop/Projects/system_manager/system_manager/GUI/src/build/gui_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
